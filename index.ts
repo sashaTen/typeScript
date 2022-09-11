1b105435e1c6182847a6d08e515d38f744dd8c8d
TEN ALEXANDER <sasha1keshten@gmail.com>
2022-09-11 18:51:27 +0900

    types

---
+++ index.ts
@@ -0,0 +1,11 @@
+let    firstName    :   string   =   "Sasha "
+let    list   :  string[] = []
+list.push('hello')
+list.push('hello')
+list.push('hello')
+list.push('hello')
+console.log(list);
+
+
+
+
