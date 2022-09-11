9fb47bb8f9c3ce62583b19df909a63931c043260
TEN ALEXANDER <sasha1keshten@gmail.com>
2022-09-11 19:04:53 +0900

    finished   for   today

--- index.ts
+++ index.ts
@@ -6,6 +6,37 @@ list.push('hello')
 list.push('hello')
 console.log(list);
 
+let   numbers  =  [1,23,6,5,4]
+numbers.push(1000000)
+console.log(numbers);
+let ourTuple: [number, boolean, string];
+ourTuple =  [74  ,     true  ,    'gg']
+
+
+
+const   carObject  :   {    name   : string    ,   year   :   number}={
+    name   :  "volve",
+    year :   1995
+}
+
+
+
+carObject.name =  "hynday"
+console.log(carObject)
+
+
+/*
+
+the  big   takeaway  from    it
+are   :  
+arrys  are very    strict of theit  type
+tuples    are  arrays  were   you can  define   different   types
+like    [number   ,  boolean   ,  string]
+objects     have    their   keys  tied to  datatypes 
+those types   can   be  replaced   only  with   values  of the  same   type
+*/
+
+
 
 
 
